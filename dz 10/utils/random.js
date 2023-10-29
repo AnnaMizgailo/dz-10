63c8887a665c87aa3d416fd062dfcc9f1f542800
AnnaMizgailo <102236391+AnnaMizgailo@users.noreply.github.com>
2023-10-29 23:39:17 +0300

    Delete dz 10 directory

--- dz 10/utils/random.js
+++
@@ -1,9 +0,0 @@
-function getRandomInt(min, max){
-    return Math.floor(Math.random()*min + (max-min-1));
-}
-
-function getRandomElement(array){
-    return array[getRandomInt(0, array.length-1)];
-}
-
-module.exports = {getRandomInt, getRandomElement};
